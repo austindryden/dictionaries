1701ac34de8ee9023baaeb50dd1aad6f98897ef1
Austin Dryden <austinisnorobot@gmail.com>
2019-11-14 14:19:24 -0500

    first commit

---
+++ dictionaries.py
@@ -0,0 +1,49 @@
+# dictionaries are collections of pairs of keys and values
+#use the key as an index to access the value.
+
+#the .get() method will not raise an exception if it can't find the key
+#.get() can take in a second argument, that will be returned if key can't be found
+
+# How do I store multiple values for a single key
+bars = {
+    'LLoyds' : {
+        'item' : 'Cheap Bourbon',
+        'day' : 'Tuesday'
+        },
+    'Manuels' : {
+        'item' : 'Dogzilla',
+        'day' : 'Wednesday'
+        },
+    'The Imperial' :{
+        'item' : 'Philly',
+        'day' : ['Tuesday', 'Friday']
+        },
+        'El Myr' :{
+            'item' : 'grizz',
+            'day' : 'Every Day'
+        }
+    }
+
+places = {
+    "US":{
+        "Georgia":{
+            "Atlanta" : {
+                "work" : "DigitalCrafts"
+            }
+        }
+    }
+}
+
+
+# How do I store more complicated data for a single key?
+
+# How do I store new values to an existing dictionary?
+
+# How do I access information in useful dictionaries?
+# how do I access nested information?
+# how do I loop through information in a dictionay?
+
+
+# How do I modify a dictionary?# How do I store new values to n existing dictionary?
+# how do I remove value values from an existing dictionary?
+
